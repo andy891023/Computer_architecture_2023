a56ffa6ff3d2fbdefee4ae4b6a69eac04353d162
andy891023 <145583185+andy891023@users.noreply.github.com>
2023-12-01 22:01:48 +0800

    Delete hw3/Execution_of_Single_Cycle_CPU_should_execute_correctly directory

--- hw3/Execution_of_Single_Cycle_CPU_should_execute_correctly/verilated/Execute-harness.d
+++
@@ -1,6 +0,0 @@
-Execute-harness.o: ../Execute-harness.cpp VExecute.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_cov.h VExecute.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h
--- hw3/Execution_of_Single_Cycle_CPU_should_execute_correctly/verilated/VExecute__ALLsup.d
+++
@@ -1,7 +0,0 @@
-VExecute__ALLsup.o: VExecute__ALLsup.cpp VExecute.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_cov.h VExecute__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h VExecute__Syms.h VExecute.h \
- VExecute__Syms.cpp VExecute__Trace__Slow.cpp
