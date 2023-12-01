923190715a97c09c515569758d4392c0b4d0ab02
andy891023 <145583185+andy891023@users.noreply.github.com>
2023-12-01 22:02:03 +0800

    Delete hw3/InstructionFetch_of_Single_Cycle_CPU_should_fetch_instruction directory

--- hw3/InstructionFetch_of_Single_Cycle_CPU_should_fetch_instruction/verilated/VInstructionFetch__ALLsup.d
+++
@@ -1,9 +0,0 @@
-VInstructionFetch__ALLsup.o: VInstructionFetch__ALLsup.cpp \
- VInstructionFetch.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_cov.h \
- VInstructionFetch__Trace.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h VInstructionFetch__Syms.h \
- VInstructionFetch.h VInstructionFetch__Syms.cpp \
- VInstructionFetch__Trace__Slow.cpp
